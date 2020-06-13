fae4fe55a6ce0ff5308e62eff398b7e5757169e8
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-06-13 17:11:15 +0800

    Set application permissions of find & findOne to true on initial install

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -10,4 +10,26 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+    const authenticated = await strapi.query('role', 'users-permissions').findOne({ type: 'authenticated' });
+    const unauthenticated = await strapi.query('role', 'users-permissions').findOne({ type: 'public' });
+
+    const setPermissions = (permission) => {
+
+        const isFindAction = permission.action === "findone" || permission.action === "find"
+        const isApplicationType = permission.type === 'application'
+
+        if (isApplicationType && isFindAction){ 
+            let newPermission = permission;
+            newPermission.enabled = true; 
+
+            strapi.query('permission', 'users-permissions').update( { id: newPermission.id }, newPermission ); 
+        }
+    }
+
+    authenticated.permissions.forEach(setPermissions);
+    unauthenticated.permissions.forEach(setPermissions);
+
+    return;
+}; 
+
